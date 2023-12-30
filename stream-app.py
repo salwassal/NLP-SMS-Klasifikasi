0dccace2c4a4de5515a481afc6462322026776f2
Salwa <75198483+salwassal@users.noreply.github.com>
2023-12-30 23:05:41 +0700

    Update stream-app.py

--- stream-app.py
+++ stream-app.py
@@ -3,10 +3,10 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Load save model
-model_fraud = pickle.load(open('/content/drive/MyDrive/NLP-SMS/dataset/model_fraud.sav','rb'))
+model_fraud = pickle.load(open('dataset/model_fraud.sav','rb'))
 
 tfidf = TfidfVectorizer
-loaded_vec = TfidfVectorizer(decode_error="replace", vocabulary=set(pickle.load(open("/content/drive/MyDrive/NLP-SMS/dataset/new_selected_features_tf-idf.sav", "rb"))))
+loaded_vec = TfidfVectorizer(decode_error="replace", vocabulary=set(pickle.load(open("dataset/new_selected_features_tf-idf.sav", "rb"))))
 
 # Judul halaman web
 st.title("Prediksi SMS Penipuan")
